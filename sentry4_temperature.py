0e04eb8fa7c191f27274802736b7d2681e782241
Curtis Bowden <curtis.bowden@gmail.com>
2022-07-08 21:26:17 +0000

    test

--- sentry4_temperature.py
+++ sentry4_temperature.py
@@ -16,7 +16,7 @@ def parse_sentry4_temperature(string_table):
             # No sensor connected
             continue
         else:
-            item = 'Temperature ' + sensor_id + ' ' + name.replace('(', '[').replace(')', ']')
+            item = 'Temperature ' + sensor_id + ' ' + name
             parsed[item] = {}
             parsed[item]['Value'] = float(int(value)/10)
             parsed[item]['Status'] = int(status)
